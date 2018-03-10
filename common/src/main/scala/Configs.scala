2140bb85d9afadc6ebd6fc39bfb88fd3a8cd9742
guangyuanh <thu.guangyuan12@gmail.com>
2018-03-10 12:13:01 -0500

    add simple accum

--- common/src/main/scala/Configs.scala
+++ common/src/main/scala/Configs.scala
@@ -59,6 +59,8 @@ class NoBrPred extends Config((here, site, up) => {
   })
 
 class ZynqConfig extends Config(new WithZynqAdapter ++ new WithNBigCores(1) ++ new DefaultFPGAConfig)
+class RoccZynqConfig extends Config(new WithZynqAdapter ++ new WithNBigCores(1) ++ new RoccExampleConfig ++ new DefaultFPGAConfig)
+class SimpleRoccZynqConfig extends Config(new WithZynqAdapter ++ new WithNBigCores(1) ++ new WithRoccExample ++ new DefaultFPGAConfig)
 class ZC706MIGConfig extends Config(new WithExtMemSize(0x40000000L) ++ new ZynqConfig)
 class BOOMZynqConfig extends Config(new WithExtMemSize(0x40000000L) ++ new WithZynqAdapter ++ new boom.MediumBoomConfig)
 class SmallBOOMZynqConfig extends Config(new WithZynqAdapter ++ new boom.SmallBoomConfig)
